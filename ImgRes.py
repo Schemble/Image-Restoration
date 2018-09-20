89c726042f329eeb8c2abd2aa3087044b8f73af1
John-macbook <johnringdahl@Johns-MacBook-Pro.local>
2018-09-20 22:29:56 +0200

    Working Euler
    Forward Euler is working
    slow w/ big pictures

--- ImgRes.py
+++ ImgRes.py
@@ -9,35 +9,57 @@ class ImageRestoration:
     def __init__(self, img, mask):
         
         self.image=self.loadimage(img)
+        self.original=self.image.copy()
         self.mask=self.loadmask(mask)
                 
     
     def loadimage(self, img, gs=0):
         #add grayscale conversion in if
-        return imread(img, pilmode='F')
+        return imread(img, as_gray=1)
     
     def loadmask(self, img):
         #add mask conditions
         return imread(img, pilmode='F')
     
     def showimage(self):
-        plt.imshow(img)
+        plt.figure()
+        plt.imshow(self.image, cmap='gray')
     
     def showmask(self):
-        plt.imshow(mask)
+        plt.figure()
+        plt.imshow(self.mask, cmap='gray')
         
-    def analyzemask(self, m):
+    def showoriginal(self):
+        plt.figure()
+        plt.imshow(self.original, cmap='gray')
+        
+    def __analyzemask(self, m):
         D_ind=[]
         for i in range(shape(m)[0]):
             for j in range(shape(m)[1]):
-                if m[i, j]!=0:
-                    Dind.append([i, j])
-        return D_ind
+                if m[i, j]!=255:
+                    D_ind.append([i, j])
+        return array(D_ind)
 
-#    def __Euler(self, u, D, h, a):
-#        
-#        for i in range(shape(u)[0]):
-#            for j in range(shape(u)[1]):
-#                u[i, j]+=D*h/a**2*(u[i-1, j]+u[i+1, j]+u[i, j-1]+u[i, j+1]-(4-B)*u[i,j])
-# 
-#       return u
+    def __EulerStep(self,D_ind, u, D, h, a=1):
+        #Problem with mask-points at the border of the frame 
+        for ind in D_ind:
+
+            u[ind[0], ind[1]]+=D*h/a**2*(u[ind[0]-1, ind[1]]+u[ind[0]+1, ind[1]]+u[ind[0], ind[1]-1]+u[ind[0], ind[1]+1]-4*u[ind[0], ind[1]])
+        return u
+    def restore(self):
+        D_ind=self.__analyzemask(self.mask)
+        u=self.image
+        u_p=u.copy()
+
+        u=self.__EulerStep(D_ind, u, 1, 0.1)
+        while any(u!=u_p):
+            print(1)
+            u_p=u.copy()
+            u=self.__EulerStep( D_ind, u, 1, 0.1)
+        plt.figure()
+        plt.imshow(u, cmap='gray')
+        
+imgres=ImageRestoration('flower_destroyed.jpg','Mask1.jpg')
+imgres.restore()
+    
